1525d77878fcfe69e6aad17e5bf3a7354a7863f9
Your Name <you@example.com>
2018-10-15 15:15:07 +1100

    add rails app

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  resources :ingredients
+  resources :recipes do
+    member do 
+      post 'toggle_status', to:  'recipes#toggle_status'
+    end
+end
+
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
